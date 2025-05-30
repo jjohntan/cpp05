e7dab397d88811e1ed2cb59b4d31e53ccdbfa566
Jek John Tan <jetan@u90z02s03.42kl.edu.my>
2025-05-30 18:27:47 +0800

    rename Form to AForm

---
+++ ex02/AForm.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AForm.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 18:58:47 by jetan             #+#    #+#             */
+/*   Updated: 2025/05/30 16:48:59 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AFORM_HPP
+# define AFORM_HPP
+
+#include <string>
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+//abstract class
+class AForm
+{
+	private:
+		const std::string name;
+		bool isSigned;
+		const int gradeToSign;
+		const int gradeToExecute;
+	public:
+		AForm();
+		AForm(std::string _name, int _gradeTosign, int _gradeToExecute);
+		AForm(const AForm &other);
+		AForm &operator=(const AForm &other);
+		~AForm();
+		std::string getName() const;
+		bool getIsSigned() const;
+		int getGradeToSign() const;
+		int getGradeToExecute() const;
+		void beSigned(Bureaucrat const &bureaucrat);
+		virtual void execute(Bureaucrat const & executor) const = 0;//pure virutal function
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &os, const AForm &obj);
+
+#endif
