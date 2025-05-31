f8397b34116b22e60aab01ead8b0c46b43645cd9
Jek John Tan <jetan@u90z02s01.42kl.edu.my>
2025-05-31 16:56:24 +0800

    add PPF class

---
+++ ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 19:03:32 by jetan             #+#    #+#             */
+/*   Updated: 2025/05/31 16:55:48 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm: public AForm
+{
+	private:
+		std::string target;
+	public:
+		PresidentialPardonForm();
+		PresidentialPardonForm(std::string _target);
+		PresidentialPardonForm(const PresidentialPardonForm &other);
+		PresidentialPardonForm &operator=(const PresidentialPardonForm &other);
+		~PresidentialPardonForm();
+		void execute(Bureaucrat const & executor);
+
+};
+
+#endif
