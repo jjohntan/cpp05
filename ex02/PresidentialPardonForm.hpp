d71116f0b15ebfea374c5953552249480a2f928c
Jek John Tan <jetan@u91z02s01.42kl.edu.my>
2025-06-05 16:46:36 +0800

    implemen ppf getter and execute

--- ex02/PresidentialPardonForm.hpp
+++ ex02/PresidentialPardonForm.hpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 19:03:32 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/03 16:38:56 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/05 16:30:47 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ class PresidentialPardonForm: public AForm
 		PresidentialPardonForm(const PresidentialPardonForm &other);
 		PresidentialPardonForm &operator=(const PresidentialPardonForm &other);
 		~PresidentialPardonForm();
+		std::string getTarget() const;//getter
 		void execute(Bureaucrat const & executor) const;
 
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,36 +6,50 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 16:22:59 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/05 16:24:01 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/05 16:36:30 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main()
 {
+	// try
+	// {
+	// 	Bureaucrat bob("Bob", 45);
+	// 	ShrubberyCreationForm formA("home");
+	// 	bob.signForm(formA);
+	// 	formA.execute(bob);
+	// }
+	// catch (std::exception & e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	// try
+	// {
+	// 	Bureaucrat Jim("Jim", 45);
+	// 	RobotomyRequestForm formB("home");
+	// 	Jim.signForm(formB);
+	// 	formB.execute(Jim);
+	// }
+	// catch (std::exception & e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
 	try
 	{
-		Bureaucrat bob("Bob", 45);
-		ShrubberyCreationForm formA("home");
+		Bureaucrat bob("Bob", 4);
+		PresidentialPardonForm formA("home");
 		bob.signForm(formA);
 		formA.execute(bob);
+
 	}
-	catch (std::exception & e)
+	catch(const std::exception& e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cerr << e.what() << '\n';
 	}
-	try
-	{
-		Bureaucrat Jim("Jim", 45);
-		RobotomyRequestForm formB("home");
-		Jim.signForm(formB);
-		formB.execute(Jim);
-	}
-	catch (std::exception & e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	
 }
