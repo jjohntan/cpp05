ba79b7f601313599eacb13576ab1cd354eb7c42b
Jek John Tan <jetan@u90z02s03.42kl.edu.my>
2025-06-10 20:59:52 +0800

    implement Bureacrat executeForm()

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 16:22:59 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/06 17:54:59 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/10 20:54:13 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,50 @@ void testSignForm()
 	std::cout << "-------------valid---------------" << std::endl;
 	try
 	{
-		
+		Bureaucrat a("Bob", 136);
+		ShrubberyCreationForm forma("house");
+		a.signForm(forma);
+		a.executeForm(forma);
 	}
 	catch (std::exception & e)
 	{
 		std::cout << e.what() << std::endl;
 	}
-	std::cout << "\n-------------invalid-------------" << std::endl;
-	try
-	{
-		
-	}
-	catch (std::exception & e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	// try
+	// {
+	// 	Bureaucrat b("", 44);
+	// 	RobotomyRequestForm formb("");
+	// 	b.signForm();
+	// 	formb.execute();
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	// try
+	// {
+	// 	Bureaucrat c("", 4);
+	// 	PresidentialPardonForm formc("");
+	// 	c.signForm();
+	// 	formc.execute();
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+
+	// std::cout << "\n-------------invalid-------------" << std::endl;
+	// try
+	// {
+	// 	Bureaucrat jim("Jim", 150);
+	// 	RobotomyRequestForm formb("house");
+	// 	// jim.signForm(formb);
+	// 	formb.execute(jim);
+	// }
+	// catch (std::exception & e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
 }
 
 void OrthodoxCanonicalForm()
@@ -52,6 +81,6 @@ void OrthodoxCanonicalForm()
 
 int main()
 {
-	OrthodoxCanonicalForm();
+	// OrthodoxCanonicalForm();
 	testSignForm();
 }
